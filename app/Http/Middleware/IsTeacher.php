dc347d9910236fa08b46f79f0ccce70247e6e4d3
M. Dafitra A <muhamad.odd@gmail.com>
2019-10-24 12:43:55 +0700

    New Middleware

---
+++ app/Http/Middleware/IsTeacher.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsTeacher
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()->role === 1) return $next($request);
+        else return response()->json(['error' => 'Unauthorized'], 403);
+    }
+}
