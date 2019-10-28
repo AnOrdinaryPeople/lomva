1e8c08419fda6a5c24ff96aa11958d0252209e16
M. Dafitra A <muhamad.odd@gmail.com>
2019-10-28 12:04:27 +0700

    Changed title

--- public/js/app.js
+++ public/js/app.js
@@ -3066,7 +3066,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
-      appName: "Forum BK",
+      appName: "H.O.P.E",
       height: 0
     };
   },
