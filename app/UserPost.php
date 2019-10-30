adbbec60c7b152b36e1d321992794d5ca71eb704
M. Dafitra A <muhamad.odd@gmail.com>
2019-10-30 09:13:11 +0700

    Changed function get to paginate

--- app/UserPost.php
+++ app/UserPost.php
@@ -12,35 +12,38 @@ class UserPost extends Model
     public function user(){
     	return $this->belongsTo(\App\User::class);
     }
+    public function comment(){
+    	return $this->hasMany(\App\Comment::class);
+    }
     public static function getPost($id){
     	return DB::table('user_posts')
-    		->select('title', 'desc', 'name', 'avatar', 'role')
+    		->select('title', 'desc', 'name', 'avatar', 'role', 'user_posts.created_at as date_post')
     		->join('users', 'user_id', '=', 'users.id')
     		->where('user_posts.id', $id)
     		->first();
     }
     public static function allPost($check){
     	$db = DB::table('user_posts')
-    		->select('user_posts.id as toId', 'title', 'name')
+    		->select('user_posts.id as toId', 'title', 'name', 'user_posts.created_at as date_post')
     		->join('users', 'user_id', '=', 'users.id')
     		->orderBy('user_posts.id', 'desc');
 
-    	if($check == 'with-bk') return $db->get();
-    	else return $db->where('type', 0)->get();
+    	if($check == 'with-bk') return $db->paginate(10);
+    	else return $db->where('type', 0)->paginate(10);
     }
     public static function getUserPost($id){
     	return DB::table('user_posts')
     		->select('id', 'title')
     		->where('user_id', $id)
-    		->get();
+    		->paginate(10);
     }
     public static function search($q, $role){
     	$db = DB::table('user_posts')
-    		->select('user_posts.id as toId', 'title', 'name')
+    		->select('user_posts.id as toId', 'title', 'name', 'user_posts.created_at as date_post')
     		->join('users', 'user_id', '=', 'users.id')
     		->where('title', 'like', '%'.$q.'%');
 
-    	if($role === 1) return $db->get();
-    	else return $db->where('type', 0)->get();
+    	if($role === 1) return $db->paginate(10);
+    	else return $db->where('type', 0)->paginate(10);
     }
 }
