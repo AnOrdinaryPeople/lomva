dd295e8862fb517165572f3100f1808e93b8cbc5
M. Dafitra A <muhamad.odd@gmail.com>
2019-11-01 12:23:59 +0700

    Added new component

--- resources/js/router.js
+++ resources/js/router.js
@@ -10,6 +10,11 @@ import Guide from './components/Guide'
 import UserHome from './components/UserHome'
 import Post from './components/Post'
 import MyPost from './components/MyPost'
+import Questionnaire from './components/quest/Questionnaire'
+import InfoQuest from './components/quest/InfoQuest'
+import QuestTest from './components/quest/QuestTest'
+import QuestTeacher from './components/quest/QuestTeacher'
+import CreateQuest from './components/quest/CreateQuest'
 
 export default new VueRouter({
 	history: true,
@@ -58,6 +63,48 @@ export default new VueRouter({
 			name: 'edit-my-post',
 			component: AddPost,
 			meta: { auth: true }
+		},{
+			path: '/questionnaire',
+			name: 'questionnaire',
+			component: Questionnaire,
+			meta: { auth: {
+				roles: 0,
+				redirect: '/dashborad'
+			}}
+		},{
+			path: '/questionnaire/:id',
+			name: 'info-quest',
+			component: InfoQuest,
+			meta: { auth: true }
+		},{
+			path: '/questionnaire/:id/test',
+			name: 'quest-test',
+			component: QuestTest,
+			meta: { auth: true }
+		},{
+			path: '/teacher/questionnaire',
+			name: 'teacher-quest',
+			component: QuestTeacher,
+			meta: { auth: {
+				roles: 1,
+				redirect: '/questionnaire'
+			}}
+		},{
+			path: '/teacher/questionnaire/create',
+			name: 'teacher-quest-create',
+			component: CreateQuest,
+			meta: { auth: {
+				roles: 1,
+				redirect: '/questionnaire'
+			}}
+		},{
+			path: '/teacher/questionnaire/:id/edit',
+			name: 'teacher-quest-edit',
+			component: CreateQuest,
+			meta: { auth: {
+				roles: 1,
+				redirect: '/questionnaire'
+			}}
 		},{
 			path: '*',
 			redirect: { name: 'home' }
