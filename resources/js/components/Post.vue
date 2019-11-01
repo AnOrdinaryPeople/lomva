3233956dd47d7d42022e3746850d314f9f6366ae
M. Dafitra A <muhamad.odd@gmail.com>
2019-11-01 12:21:28 +0700

    Updated component

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -1,12 +1,19 @@
 <template>
 	<div class="bg-white p-4">
 		<div class="row">
-			<div class="col-8">
+			<div class="col-9">
 				<h1 class="m-0">{{ content.title }}</h1>
 				<small>{{ dateFill(content.date_post) }}</small>
 			</div>
-			<div class="col-4 text-right">
-				<p>{{ content.name }} <img class="img-fluid rounded-circle" :src="content.avatar || sauce+'/img/default.png'" width="40" height="40"></p>
+			<div class="col-3">
+				<div class="row">
+					<div class="col-sm-6 col-md-9 col-lg-9 pt-2 text-right">
+						<p>{{ content.name }}</p>
+					</div>
+					<div class="col-sm-6 col-md-3 col-lg-3 pl-0">
+						<img class="rounded-circle" :src="content.avatar || sauce+'/img/default.png'" width="40" height="40">
+					</div>
+				</div>
 			</div>
 		</div>
 		<hr/>
@@ -25,7 +32,7 @@
 		<laravel-vue-pagination :data="reply" @pagination-change-page="changePage"></laravel-vue-pagination>
 		<div v-if="$auth.check()" class="form-group">
 			<textarea class="form-control" placeholder="Isi komentar.." style="height: 100px" v-model="replyTxt"></textarea>
-			<button class="mt-2 btn" :class="$auth.user().role === 1 ? 'btn-success' : 'btn-primary'" @click="sendReply">Kirim komentar</button>
+			<button class="mt-2 btn" :class="$auth.user().role === 1 ? 'btn-success' : 'btn-primary'" @click.prevent="sendReply">Kirim komentar</button>
 		</div>
 		<div v-else class="text-center">
 			<h1><router-link to="/login">Login</router-link> untuk menambah komentar</h1>
@@ -51,7 +58,7 @@
 			</div>
 		</div>
 		<div v-if="$auth.check()" id="edit-reply" class="modal fade" tabindex="-1">
-			<div class="modal-dialog">
+			<div class="modal-dialog modal-dialog-scrollable">
 				<div class="modal-content">
 					<div class="modal-header">
 						<h5>Edit komentar</h5>
@@ -64,7 +71,7 @@
 					</div>
 					<div class="modal-footer">
 						<button class="btn btn-secondary" data-dismiss="modal">Batalkan</button>
-						<button class="btn":class="$auth.user().role === 1 ? 'btn-success' : 'btn-primary'" @click.prevent="editSend">Update komentar</button>
+						<button class="btn" :class="$auth.user().role === 1 ? 'btn-success' : 'btn-primary'" @click.prevent="editSend">Update komentar</button>
 					</div>
 				</div>
 			</div>
@@ -110,7 +117,10 @@
 			sendReply(){
 				if(this.replyTxt != '')
 					axios.post(`/get-post/${this.$route.params.id}/send-reply/${this.$auth.user().id}`, {reply: this.replyTxt})
-						.then(resp => this.reply = resp.data)
+						.then(resp => {
+							this.reply = resp.data
+							this.replyTxt = ''
+						})
 						.catch(err => console.error(err))
 			},
 			openEdit(desc, id){
