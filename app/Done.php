f7aea22ebff0dbcd7695fc8a0290fb7f45ee8423
M. Dafitra A <muhamad.odd@gmail.com>
2019-11-03 12:18:33 +0700

    Added new model

---
+++ app/Done.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Done extends Model
+{
+    protected $fillable = ['user_id', 'total_score', 'quest_id'];
+
+    public function questionnaire(){
+    	return $this->belongsTo(\App\Questionnaire::class);
+    }
+
+    public static function testCheck($id){
+    	return DB::table('dones')
+    		->join('questionnaires', 'questionnaires.id', '=', 'quest_id')
+    		->where('dones.user_id', $id)
+    		->count();
+    }
+}
